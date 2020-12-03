b4abcafe3fb83abdff353b3c286689fa3d19c253
Jacob Brink <jacobpeterbrink@gmail.com>
2020-12-03 14:36:00 -0500

    Removed title test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { ItemCardComponent } from './components/item-card/item-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
@@ -12,17 +11,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { QuizComponent } from './pages/quiz/quiz.component';
-import { QuestionComponent } from './components/question/question.component';
-import { ImageSelectCardComponent } from './components/image-select-card/image-select-card.component';
+import { ItemCardComponent } from './components/item-card/item-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemCardComponent,
-    QuizComponent,
-    QuestionComponent,
-    ImageSelectCardComponent
   ],
   imports: [
     BrowserModule,
