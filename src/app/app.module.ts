15b4614d6b33aa118aaedc7aa9a070c3e179f15f
Jacob Brink <jacobpeterbrink@gmail.com>
2020-12-03 20:10:41 -0500

    basic functionality working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { PageComponent } from './components/page/page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageComponent,
   ],
   imports: [
     BrowserModule,
