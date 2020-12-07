71ac058658d55b563c095df1674ececa8c5fec99
Jacob Brink <jacobpeterbrink@gmail.com>
2020-12-06 21:10:32 -0500

    Fixed routing error by moving wildcard route to end of list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,24 +6,26 @@ import { CollectionPageComponent } from './components/collection-page/collection
 import { QuizPageComponent } from './components/quiz-page/quiz-page.component';
 import { FinalPageComponent } from './components/final-page/final-page.component';
 
+// turns out having the wildcard at the top breaks stuff
+// https://stackoverflow.com/questions/48275600/angular-components-not-reloading-on-route-change
 const routes: Routes = [
-  { path: '**', component: MainPageComponent },
-  { path: 'main', component: MainPageComponent },
-  { path: 'collection', component: CollectionPageComponent },
-  { path: 'quiz', component: QuizPageComponent },
-  { path: 'final', component: FinalPageComponent },
+    { path: 'main', component: MainPageComponent },
+    { path: 'collection', component: CollectionPageComponent },
+    { path: 'quiz', component: QuizPageComponent },
+    { path: 'final', component: FinalPageComponent },
+    { path: '**', component: MainPageComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
 
 // routingComponents takes care of all the different components needed for different routes
 export const routingComponents = [
-  MainPageComponent,
-  CollectionPageComponent,
-  QuizPageComponent,
-  FinalPageComponent,
-];
+    MainPageComponent,
+    CollectionPageComponent,
+    QuizPageComponent,
+    FinalPageComponent,
+]; 
