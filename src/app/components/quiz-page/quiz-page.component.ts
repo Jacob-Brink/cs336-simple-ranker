02781eeacfe1e02b67de23ea2f8c306bc8ff46df
Jacob Bennett <jacobbennettn@gmail.com>
2020-12-09 17:54:51 -0500

    the reordering kind of works, only have the first card work so far

--- src/app/components/quiz-page/quiz-page.component.ts
+++ src/app/components/quiz-page/quiz-page.component.ts
@@ -50,16 +50,37 @@ export class QuizPageComponent implements OnInit {
   item1: item;
   item2: item;
 
+  currentMiddle: number = 0;
+  lastMiddle: number = 0;
   data: item[] = [this.dog1, this.dog2, this.dog3, this.dog4, this.dog5];
   ranked: item[] = [];
   display: item[] = [];
 
+  // This refreshes the whole ranked/data stuff
   ngOnInit(): void {
+    // initialize the middles
+    this.currentMiddle = 0;
+    this.lastMiddle = 0;
+
+
+    // initialize data 
+    this.data = [this.dog1, this.dog2, this.dog3, this.dog4, this.dog5];
+    
+    //add current first item to ranked array
     this.ranked.push(this.data[0]);
-    this.item1 = this.data[0];
+    
+    //add current first item to display array
+    this.display.push(this.ranked[0]);
+    
+    // delete current first item
+    this.data.shift();
+    
+    // add current second item to display
+    this.display.push(this.data[0]);
+
+    // might not need this
+    this.item1 = this.ranked[this.currentMiddle];
     this.item2 = this.data[1];
-    this.display.push(this.item1);
-    this.display.push(this.item2);
 
   }
 
@@ -69,34 +90,76 @@ export class QuizPageComponent implements OnInit {
     // We keep asking till we can get slot 2 a spot in the ranked list
 
     // 1 Middle one vs next item in unranked list
-    // •	If we pick Middle(slot 1) -> the bottom half middle(25% queartile) becomes slot 1 (go to beginning and repeat)
-    // •	If not found then we insert slot 2 beneath the middle one
-    // 2	If we pick Next item(slot2) -> the top half middle(25% quartile) becomes slot 1
+    // •	If we pick Middle(item 1) -> the bottom half middle(25% queartile) becomes slot 1 (go to beginning and repeat)
+    // •	If not found then we insert item 2 beneath the middle one
+    // 2	If we pick Next item(item 2) -> the top half middle(25% quartile) becomes slot 1
     // go to beginning and repeat)
-    // •	If not found then we insert slot 2 above the middle one
+    // •	If not found then we insert item 2 above the middle one
 
 
     // Update and book keeping:
 
     // Unshift data
 
-    // Slot 1 = new middle(round up)
+    // item 1 = new middle(round up)
 
-    // Slot 2 = next item in data
+    // item 2 = next item in data
 
 
-  if(item == this.display[0]){
-    console.log("item1 has been picked")
-    console.log(item);
-    
-  }
-  else {
-    console.log("item 2 has been picked")
-    console.log(item);
+    if(item == this.display[0]){
+      console.log("item1 has been picked")
 
-  }
+      // if there is nothing lower than the currentMiddle item, add it directly below the current middle item.
+      if(this.currentMiddle === this.lastMiddle) {
+        console.log("there is nothing");
+        //add item 2 to right below the ranked item1
+        this.ranked.splice(this.currentMiddle + 1, 0, this.data[0]);
 
-    console.log("reorder has been called")
+        //update lastmiddle
+        this.lastMiddle = 0;
+        // update the currentmiddle
+        this.currentMiddle = parseInt(this.ranked.length / 2);
+
+        // update current display
+        this.display.pop();
+        this.data.shift();
+        this.display.push(this.data[0]);
+        
+
+
+      }
+
+      // there is something and now we need to find the new middle and item1 becomes that
+      else {
+        console.log("there is something");
+        console.log(this.ranked.length);
+        console.log(parseInt((this.currentMiddle + this.ranked.length)/2));
+
+        //update last middle
+        this.lastMiddle = this.currentMiddle;
+        this.currentMiddle = parseInt((this.ranked.length + this.currentMiddle)/2);
+
+        //replace item1 with the newmiddle item in the ranked array
+        this.display.shift();
+        this.display.splice(0, 0, this.ranked[this.currentMiddle]);
+
+
+        
+
+      }
+    }
+    else {
+      console.log("item 2 has been picked")
+
+    }
+    console.log(this.lastMiddle);
+    console.log(this.currentMiddle);
+
+    console.log("this is the rank")
+    console.log(this.ranked);
+    console.log("this is the display");
+    console.log(this.display);
+
   }
 
   
